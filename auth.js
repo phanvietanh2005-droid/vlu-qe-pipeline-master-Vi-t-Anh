743d7e66b87a49c1f6f712a882c88c59d32acb50
phanvietanh2005-droid <phanvietanh2005@gmail.com>
2026-09-19 10:58:20 +0700

    Update auth.js

--- auth.js
+++ auth.js
@@ -1,6 +1,6 @@
 function login(username, password) {
     // Validate admin login credentials
-    if (username === "admin" && password === "9999") {
+    if (username === "admin" && password === "123") {
         return true;
     }
 
